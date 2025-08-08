7d13079016e3ba38dd3a8e421764532e228c765c
Micah <166214972+back1drop@users.noreply.github.com>
2025-08-08 23:39:37 +0300

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,15 +1,18 @@
 class Book:
-    def __init__(self,title,author):
-        self.title=title
-        self.author=author
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
 class EBook(Book):
-    def __init__(self,title,author,file_size):
-        super().__init__(title,author)
-        self.file_size=file_size
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
 class PrintBook(Book):
-    def __init__(self,title,author,page_count):
-        super().__init__(title,author)
-        self.page_count=page_count
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
 class Library:
     def __init__(self):
         self.books = []
@@ -25,4 +28,3 @@ class Library:
                 print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
             else:
                 print(f"Book: {book.title} by {book.author}")
-
